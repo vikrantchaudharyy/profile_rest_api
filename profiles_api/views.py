8480e5ae38ba132a31f8a2b250ff1be0e0487f94
Vikrant Chaudhary <vikrant.96.007@gmail.com>
2020-05-07 23:45:12 +0530

    added user permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -99,5 +102,9 @@ class HelloViewSet(viewsets.ViewSet):
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
 
     queryset = models.UserProfile.objects.all()
+
+    
