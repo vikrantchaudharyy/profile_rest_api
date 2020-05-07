5cedeab42d5580c1f558d25bfa921655df445792
Vikrant Chaudhary <vikrant.96.007@gmail.com>
2020-05-07 23:32:52 +0530

    added UserProfileViewSet and UserProfileSerializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,7 +1,42 @@
 from rest_framework import serializers
-
+from profiles_api import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializers a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serialize an user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            # keys of this directory are fields you want to add custom configurations to
+            'password' : {
+                'write_only' : True,
+                'style' : {
+                    'input_type': 'password'
+                }
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+            )
+
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user acoount"""
+        if 'password' in validated_data:
+            password = validated_data.pop('passoword')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
